f49e4274172390316c7d45dca7a3b464bd1a62c9
Thomas Wilburn <twilburn@seattletimes.com>
2014-12-08 16:26:48 -0800

    Refactor for better popups, intermediate UI during layer filtering

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,10 @@
 
 require([
   "lib/esri-leaflet-clustered-feature-layer/dist/esri-leaflet-clustered-feature-layer-src",
+  "text!_popup.html",
   "lib/leaflet.markercluster/dist/leaflet.markercluster",
   "lib/component-responsive-frame/build/responsive-child"
-], function(ClusterLayer) {
+], function(ClusterLayer, popupTemplate) {
 
   var map = L.map('map').setView([39.8282, -98.5795], 5);
   L.esri.basemapLayer('Gray').addTo(map);
@@ -32,29 +33,29 @@ require([
     maxClusterRadius:50
   }).addTo(map);
 
-  // var prisons = L.esri.featureLayer(
-  //   'http://services1.arcgis.com/6blDduqElOPdymTF/arcgis/rest/services/st-pris/FeatureServer/0', {
-  //   pointToLayer: function (geojson, latlng) {
-  //     return L.marker(latlng, {
-  //       icon: icons
-  //     });
-  //   },
-  // }).addTo(map);
-
   prisons.bindPopup(function (feature) {
-    return L.Util.template('<div class="bigheader">{Prison_name}</div><div style="padding:5px;"><strong>{City}, {state}</strong></div><div class="lilheader">Industries</div><div>{Industries}</div>', feature.properties);
+    return L.Util.template(popupTemplate, feature.properties);
   });
 
+  var queryBox = document.querySelector("#query");
   var industry = document.getElementById('industry');
+  industry.value = "";
 
   industry.addEventListener('change', function(){
-    prisons.setWhere('industry_tag1 LIKE "%'+industry.value+'%" OR industry_tag2 LIKE "%'+industry.value+'%" OR industry_tag3 LIKE "%'+industry.value+'%" OR industry_tag4 LIKE "%'+industry.value+'%" OR industry_tag5 LIKE "%'+industry.value+'%"');
+    queryBox.className += " wait";
+    industry.disabled = true;
+    var query = [];
+    for (var i = 1; i <= 5; i++) {
+      query.push("industry_tag" + i + " LIKE \"%" + industry.value + "%\"");
+    }
+    query = query.join(" OR ");
+    setTimeout(function() {
+      prisons.setWhere(query, function() {
+        industry.disabled = false;
+        queryBox.className = queryBox.className.replace(/\s?wait\s?/g, "");
+      });
+    });
   });
-
-  // industry.addEventListener('change', function(){
-  //   prisons.setWhere( 'Industries LIKE "%'+industry.value+'%"');
-  //   console.log('industry_1="'+industry.value+'"')
-  // });
   
 });
 
